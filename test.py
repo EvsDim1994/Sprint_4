c774a53fa52c3a0f8d782d796c1e990e4cab9eb5
Dmitry <evsyuckov.dmitry@yandex.ru>
2025-07-21 14:55:42 +0300

    add_param_test_and_readme

--- test.py
+++ test.py
@@ -1,28 +1,52 @@
+import pytest
+
+
 class TestBooksCollector():
 
-    def test_add_new_book_with_correct_lenght(self, collector_book):
-        collector_book.add_new_book('Оно')
+    
+    @pytest.mark.parametrize('name', 
+        [
+        'Я',
+        'Очень длинное название книги, которое 40',
+        ]
+    )
+    def test_add_new_book_with_40_symbols_and_1_symblos(self, collector_book, name):
+        collector_book.add_new_book(name)
         assert len(collector_book.get_books_genre()) == 1
+
+    @pytest.mark.parametrize('name', 
+        [
+        "",
+        'Очень длинное название книги, которое содержит больше 41 символа',
+        ]
+    )
+    def test_add_new_book_with_lenght_more_than_41_symbols_and_null(self, collector_book, name):
+        collector_book.add_new_book(name)
+        assert not collector_book.get_book_genre(name)
 
     def test_add_new_book_without_genre(self, collector_book):
         collector_book.add_new_book('Война и мир')
         assert collector_book.get_book_genre('Война и мир') == ''
 
-    def test_add_new_book_with_lenght_more_than_41_symbols(self, collector_book):
-        collector_book.add_new_book('Очень длинное название книги, которое содержит больше 41 символа')
-        collector_book.set_book_genre('Очень длинное название книги, которое содержит больше 41 символа', 'Ужасы')
-        assert not collector_book.get_book_genre('Очень длинное название книги, которое содержит больше 41 символа')
+    @pytest.mark.parametrize('name, genre', 
+        [
+            ['оно', 'Ужасы'],
+            ['Звездные войны', 'Фантастика'],
+            ['Пуаро', 'Детективы'],
+            ['pinocchio', 'Мультфильмы'],
+            ['Фантомас', 'Комедии']
+        ]
+    )
+    def test_set_book_genre_for_new_book(self, collector_book, name, genre):
+        collector_book.add_new_book(name)
+        collector_book.set_book_genre(name, genre)
+        assert collector_book.get_book_genre(name) == genre
 
     def test_add_new_book_with_the_same_name(self, collector_book):
         collector_book.add_new_book('Оно')
         collector_book.set_book_genre('Оно', 'Ужасы')
         collector_book.add_new_book('Оно')
         assert len(collector_book.get_books_genre()) == 1
-
-    def test_set_book_genre_for_new_book(self, collector_book):
-        collector_book.add_new_book('Оно')
-        collector_book.set_book_genre('Оно', 'Ужасы')
-        assert collector_book.get_book_genre('Оно') == 'Ужасы'
 
     def test_get_books_with_specific_genre_get_detective(self, collector_book):
         collector_book.add_new_book('Оно')
